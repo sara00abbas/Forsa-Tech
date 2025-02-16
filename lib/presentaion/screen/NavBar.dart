f58a9732f61b0c4280d929646d7976c16c35ca99
Sedraalsabagh <sedraalsabbagh2021@gmail.com>
2025-02-16 14:59:30 +0300

    Venv

--- lib/presentaion/screen/NavBar.dart
+++ lib/presentaion/screen/NavBar.dart
@@ -1,3 +1,4 @@
+import 'package:devloper_app/presentaion/screen/widget/ListTitle.dart';
 import 'package:flutter/material.dart';
 
 class Navbar extends StatelessWidget {
@@ -7,6 +8,7 @@ class Navbar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
+        padding: EdgeInsets.zero,
         children: [
           UserAccountsDrawerHeader(
             accountName: Text("data"),
@@ -14,10 +16,19 @@ class Navbar extends StatelessWidget {
             currentAccountPicture: CircleAvatar(
               child: ClipOval(
                 child: Image.network(
-                    "https://cdn.pixabay.com/photo/2023/09/16/21/31/girl-8257551_1280.jpg"),
+                  "https://cdn.pixabay.com/photo/2023/09/16/21/31/girl-8257551_1280.jpg",
+                  width: 90,
+                  height: 90,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          )
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage("assets/images/logoForsa.PNG"))),
+          ),
+          MyListTitle(leading: "sedra", title: "sedra")
         ],
       ),
     );
---
+++ lib/presentaion/screen/widget/ListTitle.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyListTitle extends StatelessWidget {
+  final String leading;
+  final String title;
+
+  const MyListTitle({super.key, required this.leading, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Text(leading),
+      title: Text(title),
+    );
+  }
+}
